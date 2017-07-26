e4b2010c606d1ed56506fe6f7ca7db9274461be1
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-07-26 10:53:44 -0400

    update to work with validation code

---
+++ v_to_v/shadho_driver.py
@@ -0,0 +1,107 @@
+from shadho import HyperparameterSearch
+from shadho import choose, log2_randint, uniform
+
+import copy
+
+INITIALIZERS = choose(['zeros', 'ones', 'glorot_uniform',
+                      'glorot_normal'])
+
+REGULARIZERS = choose([None, 'l1', 'l2', 'l1_l2'])
+
+CONSTRAINTS = choose([None, 'non_neg', 'unit_norm'])
+
+LOSSES = choose(['squared_hinge', 'poisson', 'cosine_proximity'
+                'kullback_liebler_divergence', 'mean_squared_error'])
+
+DENSE = {
+    'units': log2_randint(4, 10),
+    'activation': choose(['elu', 'relu', 'sigmoid', 'softmax', 'tanh']),
+    'kernel_initializer': INITIALIZERS,
+    'bias_initializer': INITIALIZERS,
+    'kernel_regularizer': REGULARIZERS,
+    'bias_regularizer': REGULARIZERS,
+    'activity_regularizer': REGULARIZERS,
+    'kernel_constraint': CONSTRAINTS,
+    'bias_constraint': CONSTRAINTS,
+    'dropout': {
+        'optional': True,
+        'rate': uniform(0.0, 1.0)
+    }
+}
+
+layer1 = copy.deepcopy(DENSE)
+DENSE['optional'] = True
+layer2 = copy.deepcopy(DENSE)
+layer3 = copy.deepcopy(DENSE)
+layer4 = copy.deepcopy(DENSE)
+layer5 = copy.deepcopy(DENSE)
+
+layer1['next'] = layer2
+layer2['next'] = layer3
+layer3['next'] = layer4
+layer4['next'] = layer5
+
+space = {
+    'optimizer': choose(['sgd', 'rmsprop', 'adagrad', 'adadelta', 'adam',
+                        'nadam']),
+    'lr': uniform(0.00001, 1.0),
+    'loss': LOSSES,
+    'layers': layer1
+}
+
+
+config = {
+    'name': 'shadho',
+    'port': 9000,
+    'exclusive': True,
+    'shutdown': True,
+    'catalog': False,
+    'logfile': 'v_to_sum.log',
+    'debug': 'v_to_sum.debug',
+    'files': [
+        {
+            'localpath': 'train.py',
+            'remotepath': 'train.py',
+            'type': 'input',
+            'cache': True
+        },
+        {
+            'localpath': 'shadhocmd.sh',
+            'remotepath': 'shadhocmd.sh',
+            'type': 'input',
+            'cache': True
+        },
+        {
+            'localpath': 'train.npz',
+            'remotepath': 'train.npz',
+            'type': 'input',
+            'cache': True
+        },
+        {
+            'localpath': 'out.tar.gz',
+            'remotepath': 'out.tar.gz',
+
+        }
+    ]
+}
+
+
+if __name__ == "__main__":
+    import os
+    import json
+    opt = HyperparameterSearch(space,
+                               [],
+                               config,
+                               use_complexity=False,
+                               use_priority=False,
+                               timeout=600,
+                               max_tasks=50)
+    #opt.optimize()
+
+    for i in range(1, 301):
+        if not os.path.isdir(str(i)):
+            os.mkdir(str(i))
+
+        _, spec = opt.forest.generate()
+        with open(os.path.join(str(i), 'hyperparameters.json'), 'w') as f:
+            json.dump(spec, f)
---
+++ v_to_v2/train2.py
@@ -0,0 +1,229 @@
+#! /usr/bin/env python
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD, RMSprop
+from keras.regularizers import l1, l2, l1_l2
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import argparse, tarfile, os, tempfile, shutil, json
+
+from keras import backend as K
+
+def create_model():
+    with open('hyperparameters.json', 'r') as f:
+        spec = json.load(f)
+
+    model = Sequential()
+
+    layer = spec['layers']
+    units = layer.pop('units')
+    dropout = layer.pop('dropout', None)
+    next = layer.pop('next', None)
+
+    print(layer)
+    model.add(Dense(units, input_shape=(3,), **layer))
+    if dropout is not None and 'rate' in dropout:
+        model.add(Dropout(dropout['rate']))
+
+    layer = next
+
+    while layer is not None:
+        print(layer)
+        units = layer.pop('units')
+        dropout = layer.pop('dropout')
+        next = layer.pop('next', None)
+        model.add(Dense(
+            units,
+            input_shape=(units,),
+            **layer
+        ))
+
+        if dropout is not None and 'rate' in dropout:
+            model.add(Dropout(dropout['rate']))
+
+        layer = next
+
+    model.add(Dense(1, activation='sigmoid'))
+
+    opt = spec.pop('optimizer')
+    if opt == 'sgd':
+        opt = SGD(lr=spec['lr'])
+    elif opt == 'rmsprop':
+        opt = RMSProp(lr=spec['lr'])
+
+    model.compile(
+        optimizer=opt,
+        loss=spec['loss'],
+        metrics=[accuracy]
+    )
+
+    return model
+
+
+def accuracy(y_true, y_pred):
+    correct = K.equal(y_true - y_pred, K.zeros_like(y_true, dtype='float32'))
+    n = K.sum(K.cast(correct, 'float32'))
+    t = K.sum(K.ones_like(y_true, dtype='float32'))
+    return  n / t
+
+
+def write_output(loss, acc):
+    with open('performance.json', 'w') as f:
+        json.dump({'loss': loss ,'acc': acc}, f)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Train ANN auto-encoder.')
+    parser.add_argument('infile',
+                        help='File name for reading events.')
+
+    parser.add_argument('-o','--out', dest='outbase', metavar='OUTBASE',
+                        default='model',
+                        help='File name base (no extension) ' +
+                        'for saving model structure and weights (two separate ' +
+                        'files).')
+
+    parser.add_argument('-N','--num-epochs',
+                        default=10, type=int,
+                        help='Number of epochs')
+
+    parser.add_argument('-b','--batch-size',
+                        default=256, type=int,
+                        help='Minibatch size')
+
+    parser.add_argument('-l','--layer', dest='layers',
+                        metavar = 'NH', action='append',
+                        type=int,
+                        help='Specify a layer with %(metavar)s hidden layers.  ' +
+                        'Multiple layers can be specified')
+
+    parser.add_argument('--reg-type', choices = ['l1','l2','l1_l2'],
+                        help='Type of regularization to apply')
+
+    parser.add_argument('--reg-penalty',type=float, default=0.001,
+                        help='Regularization penalty')
+
+    def restricted_float(x):
+        x = float(x)
+        if x < 0.0 or x > 1.0:
+            raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
+        return x
+
+    parser.add_argument('--train-fraction',type=restricted_float,
+                        default = 0.9,
+                        help='Fraction (between 0. and 1.) of the examples in '+
+                        'the input file to use for training.  The rest is used '+
+                        'for testing.')
+
+    args = parser.parse_args()
+
+    # Keep track of all the output files generate so they can be
+    # stuffed into a tar file.  (Yes, a tarfile.  I'm old, OK?)
+    outFileList = []
+    tmpDirName = tempfile.mkdtemp()
+
+    # Load the data
+    npfile = np.load(args.infile)
+
+    inputs = npfile['inputs']
+    outputs = npfile['outputs']
+
+    # Standardize the input so that it has mean 0 and std dev. of 1.  This helps
+    # tremendously with training performance.
+    # inputMeans = inputs[0:int(inputs.shape[0]*args.train_fraction),:].mean(axis=0)
+    # inputStdDevs = inputs[0:int(inputs.shape[0]*args.train_fraction),:].std(axis=0)
+    # inputs = (inputs-inputMeans)/inputStdDevs
+    # outputMeans = outputs[0:int(outputs.shape[0]*args.train_fraction)].mean(axis=0)
+    # outputStdDevs = outputs[0:int(outputs.shape[0]*args.train_fraction)].std(axis=0)
+    # outputs = (outputs-outputMeans)/outputStdDevs
+
+    inputMeans = inputs.mean(axis=0)
+    inputStdDevs = inputs.std(axis=0)
+    inputs = (inputs-inputMeans)/inputStdDevs
+    outputMeans = outputs.mean(axis=0)
+    outputStdDevs = outputs.std(axis=0)
+    outputs = (outputs-outputMeans)/outputStdDevs
+
+    npFileName = 'std.npz'
+    outFileList.append(npFileName)
+    np.savez_compressed(os.path.join(tmpDirName,npFileName),
+                        inputMeans=inputMeans,
+                        inputStdDevs=inputStdDevs,
+                        outputMeans=outputMeans,
+                        outputStdDevs=outputStdDevs)
+
+    if False:
+        # Initialize the appropriate regularizer (if any)
+        reg = None
+        if args.reg_type == "l1":
+            reg = l1(args.reg_penalty)
+        elif args.reg_type == "l2":
+            reg = l1(args.reg_penalty)
+        elif args.reg_type == "l1_l2":
+            reg = l1_l2(args.reg_penalty)
+
+        # Check the requested layers.  If none, make the simplest
+        # possible: 1 layer with number of nodes equal to the size of the
+        # input.
+        if hasattr(args,'layers') and args.layers != None:
+            layers = args.layers
+        else:
+            layers = [inputs.shape[1]]
+
+
+        # Build a model
+        model = Sequential()
+        #print layers
+        # First layer
+        model.add(Dense(1,input_dim=3))
+        model.add(Activation('linear'))
+
+        model.compile(loss='mse',
+                      optimizer='adam')
+
+    model = create_model()
+    train_split = int(inputs.shape[0] * args.train_fraction)
+    Add callbacks
+    filepath = 'model.h5'
+    outFileList.append(filepath)
+    checkpoint = ModelCheckpoint(os.path.join(tmpDirName,filepath), monitor = 'val_loss', mode = 'min', save_best_only = True)
+    model.summary()
+    hist = model.fit(
+        inputs,
+        outputs,
+        validation_split=1.0 - args.train_fraction,
+        batch_size=128,
+        epochs=100
+    )
+
+    # loss = model.evaluate(inputs[train_split:], outputs[train_split:], batch_size=256)
+    # write_output(loss, 0)
+    # j = model.to_json()
+    # with open('model.json', 'w') as f:
+    #     json.dump(j, f)
+    # model.save_weights('weights.h5')
+
+
+    # hist = model.fit(inputs, outputs, validation_split=(1-args.train_fraction),
+    #           epochs=args.num_epochs, batch_size=args.batch_size, verbose=2, callbacks=[checkpoint])
+
+    print 'Tarring outfiles...'
+    outfile_name = '{}_N{}_b{}_l{}_frac{:f}'.format(args.outbase,
+                                                  100,
+                                                  128,
+                                                  len(model.layers),
+                                                  0.85)
+    if hasattr(args,'reg_type') and args.reg_type != None:
+        outfile_name += ('{}{:f}'.format(args.reg_type,args.reg_penalty))
+
+    outfile_name += '.tgz'
+
+    with tarfile.open(outfile_name,'w:gz') as tar:
+        for f in outFileList:
+            tar.add(os.path.join(tmpDirName,f),f)
+
+        shutil.rmtree(tmpDirName)
+
+    print 'Done.'
